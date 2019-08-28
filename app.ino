3b52aee47018be5a41277b771d83b8b62a7e1249
lucasdavalle <48356540+lucasdavalle@users.noreply.github.com>
2019-08-28 14:26:50 -0300

    lectura de sensores

--- app.ino
+++ app.ino
@@ -1,5 +1,4 @@
 #include "header.h"
-
 /*
 ITS VILLADA - INET - GRUPO 7 - 28/08/2019
 INTEGRANTES:
@@ -7,12 +6,21 @@ INTEGRANTES:
 - Philippeaux, Enrique
 */
 
+
 void setup() {
   // put your setup code here, to run once:
-
+Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    
+  float a =TEMP_leer();
+  Serial.println("Temperatura: " + String(a));
+
+  float b =HUM_leer();
+  Serial.println("humedad: " + String(b));
+
+  uint16_t c =LDR_leer();
+  Serial.println("lux: " + String(c));
+
+  delay(2000);
 }
--- header.h
+++ header.h
@@ -1,9 +1,9 @@
 #ifndef header_h
 #define header_h
 
-#define PIN_LM35 A0
+#define PIN_LM35 A2
 #define PIN_HUMEDAD A1
-#define PIN_LDR A2
+#define PIN_LDR A0
 
 #define PIN_RELAY_MOTOR         3
 #define PIN_RELAY_CALEFACTOR    4
