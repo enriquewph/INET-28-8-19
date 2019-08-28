532f9fc571ec11f1bea23c2e6ec9fdbbbd0f2f7e
Enrique Walter Philippeaux <quique18c@gmail.com>
2019-08-28 13:32:17 -0300

    header actualizado

--- header.h
+++ header.h
@@ -1,6 +1,15 @@
 #ifndef header_h
 #define header_h
 
+#define PIN_LM35 A0
+#define PIN_HUMEDAD A1
+#define PIN_LDR A2
+
+#define PIN_RELAY_MOTOR         3
+#define PIN_RELAY_CALEFACTOR    4
+#define PIN_RELAY_RIEGO         5
+#define PIN_DIMMER_LUZ          6
+
 
 
 #endif
