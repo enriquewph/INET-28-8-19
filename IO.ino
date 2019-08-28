330327fa9ad92b2f7136c45828e07fa763bfe203
lucasdavalle <48356540+lucasdavalle@users.noreply.github.com>
2019-08-28 18:18:30 -0300

    promedios pa todo

--- IO.ino
+++ IO.ino
@@ -7,7 +7,7 @@ float TEMP_leer()
 }
 float HUM_leer()
 {
-    float humedad = (analogRead(PIN_HUMEDAD) / 10.24);
+    float humedad = (ADC_LEER_PROMEDIO(PIN_HUMEDAD,10) / 10.24);
     return (humedad);
 }
 uint16_t LDR_leer()
@@ -16,7 +16,7 @@ uint16_t LDR_leer()
     const int B = 11;    //Resistencia a la luz (10 Lux) en KΩ
     const int Rc = 10;   //Resistencia calibracion en KΩ
 
-    int valor_ldr = analogRead(PIN_LDR);
+    int valor_ldr = ADC_LEER_PROMEDIO(PIN_LDR,10);
 
     int ilum = ((long)valor_ldr * A * 10) / ((long)B * Rc * (1024 - valor_ldr)); //usar si LDR entre A0 y Vcc (como en el esquema anterior)
     return (ilum);
