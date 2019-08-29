89fbe302fdf18aed9c15259f841b9d9ca013eb2a
Enrique Walter Philippeaux <quique18c@gmail.com>
2019-08-29 00:44:54 -0300

    agregado seteo de estados de temp por schmitt

--- salidas.ino
+++ salidas.ino
@@ -1,6 +1,5 @@
 #include "header.h"
 
-
 void control_HUMEDAD()
 {
     if (HUMEDAD <= HUMEDAD_TRIGGER && HUMEDAD_ESTADO == 0)
@@ -11,33 +10,35 @@ void control_HUMEDAD()
     {
         HUMEDAD_ESTADO = 0;
     }
-    FUNCIONAMIENTO_REGADO=HUMEDAD_ESTADO;
-    Serial.println("riego "+ String(HUMEDAD_ESTADO));
-    
+    FUNCIONAMIENTO_REGADO = HUMEDAD_ESTADO;
+    Serial.println("riego " + String(HUMEDAD_ESTADO));
 }
 void control_TEMP()
 {
     if (TEMPERATURA <= TEMPERATURA_TRIGGER && TEMPERATURA_BAJA_ESTADO == 0)
     {
         TEMPERATURA_BAJA_ESTADO = 1;
+        FUNCIONAMIENTO_TEMP = MODO_TEMP_CALENTANDO;
     }
     if (TEMPERATURA >= TEMPERATURA_RELEASE && TEMPERATURA_BAJA_ESTADO == 1)
     {
-    
+
         TEMPERATURA_BAJA_ESTADO = 0;
+        FUNCIONAMIENTO_TEMP = MODO_TEMP_STANDBY;
     }
-    
-   if (TEMPERATURA >= TEMPERATURA_TRIGGER && TEMPERATURA_ALTA_ESTADO == 0)
+
+    if (TEMPERATURA >= TEMPERATURA_TRIGGER && TEMPERATURA_ALTA_ESTADO == 0)
     {
         TEMPERATURA_ALTA_ESTADO = 1;
+        FUNCIONAMIENTO_TEMP = MODO_TEMP_ENFRIANDO;
     }
     if (TEMPERATURA <= TEMPERATURA_RELEASE && TEMPERATURA_ALTA_ESTADO == 1)
     {
-    
+
         TEMPERATURA_ALTA_ESTADO = 0;
+        FUNCIONAMIENTO_TEMP = MODO_TEMP_STANDBY;
     }
-    
-Serial.println("estado calefactor "+ String(TEMPERATURA_BAJA_ESTADO));
-Serial.println("estado extractor "+ String(TEMPERATURA_ALTA_ESTADO));
-  
+
+    Serial.println("estado calefactor " + String(TEMPERATURA_BAJA_ESTADO));
+    Serial.println("estado extractor " + String(TEMPERATURA_ALTA_ESTADO));
 }
